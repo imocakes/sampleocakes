5f29c816c04617eb966d9f8b4753bf3d9be63a69
“imocakes” <gel.1dlovers@gmail.com>
2022-09-08 13:26:04 +0800

    fifth commit

--- pages/index.js
+++ pages/index.js
@@ -42,25 +42,7 @@ export default function Home() {
           </rowColumn2>
         </div>
 
-        <div>
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer>
